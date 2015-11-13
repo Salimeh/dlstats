74e592ab4aaf0424f7ee1dab1a5094467dd384f9
Salimeh <salimeh@Salimeh-PC.lan.cepremap.org>
2015-11-13 15:37:50 +0100

    Fix period problem

--- dlstats/fetchers/IFS.py
+++ dlstats/fetchers/IFS.py
@@ -21,25 +21,25 @@ class IFS_Data():
         self.indicatore_list= {}
     def load_original_data(self):
         
-        with open('/home/salimeh/IFS/IFS_300.csv') as csvfile:
+        with open('/home/salimeh/IFS/IFS_10-20-2015 20-09-38-08.csv') as csvfile:
             reader = csv.DictReader(csvfile)
-
             for row in reader :
-                print(row)
-                
                 self.frequency = 'A'
+                plus_fre = 23 # Default base year in pandas.period is 1970, 1970-1947=23
                 if 'Q' in row["Time Period"]:
                     self.frequency = 'Q'
+                    plus_fre = 23*4
                 if 'M' in row["Time Period"]: 
-                    self.frequency = 'M'                   
+                    self.frequency = 'M' 
+                    plus_fre = 23*12
                 self.key = row["Indicator Code"]+'.'+row["Country Code"] +'.'+ self.frequency 
                 if self.key in self.attempt.keys():
 
                     self.attempt[self.key]["Country Code"] = row["Country Code"]
                     self.attempt[self.key]["Indicator Code"] = row["Indicator Code"]
                     self.attempt[self.key]["frequency"] =self.frequency
-                    self.attempt[self.key]["values"][pandas.Period(row["Time Period"],self.frequency).ordinal+23] = row["Value"]
-                    self.attempt[self.key]["Status"][pandas.Period(row["Time Period"],self.frequency).ordinal+23] = row["Status"]
+                    self.attempt[self.key]["values"][pandas.Period(row["Time Period"].replace('M', '-'),self.frequency).ordinal+plus_fre] = row["Value"]
+                    self.attempt[self.key]["Status"][pandas.Period(row["Time Period"].replace('M', '-'),self.frequency).ordinal+plus_fre] = row["Status"]
                 
                 else:
                     self.attempt[self.key] = {}
@@ -57,19 +57,17 @@ class IFS_Data():
                     self.attempt[self.key]["frequency"] =self.frequency
                     #print(self.attempt[self.key]["values"])
                     #print(self.frequency)
-                    print(row["Time Period"])
                     #print(pandas.Period(row["Time Period"],self.frequency).ordinal+23+1947)
-                    self.attempt[self.key]["values"][pandas.Period(row["Time Period"],self.frequency).ordinal+23] = row["Value"]
-                    self.attempt[self.key]["Status"][pandas.Period(row["Time Period"],self.frequency).ordinal+23] = row["Status"]
-                print(row["Country Name"])
+                    self.attempt[self.key]["values"][pandas.Period(row["Time Period"].replace('M', '-'),self.frequency).ordinal+plus_fre] = row["Value"]
+                    self.attempt[self.key]["Status"][pandas.Period(row["Time Period"].replace('M', '-'),self.frequency).ordinal+plus_fre] = row["Status"]
                 if row["Country Code"] in self.country_list.keys():
-                    break
+                    pass
                 else:
-                    self.country_list[row["Country Code"]] =  row["Country Name"]               
+                    self.country_list[row["Country Code"]] =  row['\ufeff"Country Name"']               
                     
 
                 if row["Indicator Code"] in self.indicatore_list.keys():
-                    break
+                    pass
                 else:    
                     self.indicatore_list[row["Indicator Code"]] = row["Indicator Name"]
                     
