4c98096651614e8cb5ae450d3e2e636fa93698e8
Salimeh <salimeh@Salimeh-PC.lan.cepremap.org>
2015-12-11 14:22:42 +0100

    Add data sample

---
+++ dlstats/tests/fetchers/test_imf.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 10:53:43 2015
+
+"""
+
+dataset_names = ['WEO', 'IFS']
+
+
+DATASETS['WEO']["data"] ="""WEO Country Code	ISO	WEO Subject Code	Country	Subject Descriptor	Subject Notes	Units	Scale	Country/Series-specific Notes	1980	1981	1982	1983	1984	1985	1986	1987	1988	1989	1990	1991	1992	1993	1994	1995	1996	1997	1998	1999	2000	2001	2002	2003	2004	2005	2006	2007	2008	2009	2010	2011	2012	2013	2014	2015	2016	2017	2018	2019	2020	Estimates Start After
+512	AFG	NGDP_R	Afghanistan	Gross domestic product, constant prices	Expressed in billions of national currency units; the base year is country-specific. Expenditure-based GDP is total final expenditures at purchasers? prices (including the f.o.b. value of exports of goods and services), less the f.o.b. value of imports of goods and services. [SNA 1993]	National currency	Billions	Source: National Statistics Office Latest actual data: 2013. Latest actual data is for the solar year 2013/14 Notes: National accounts data is originally compiled on the basis of a solar year, which runs from March 21 to March 20. Data is converted to calendar years for the purpose of WEO publication. National accounts manual used: System of National Accounts (SNA) 1993 GDP valuation: Market prices Start/end months of reporting year: January/December. none Base year: 2002. Base year is the solar year 2002/03 Chain-weighted: No Primary domestic currency: Afghan Afghani Data last updated: 09/2015	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	182.842	198.736	200.069	223.737	235.731	267.177	277.498	334.621	362.857	386.368	440.336	457.654	463.520	472.981	487.097	506.495	531.706	563.694	597.559	2013
+512	AFG	NGDP_RPCH	Afghanistan	Gross domestic product, constant prices	Annual percentages of constant price GDP are year-on-year changes; the base year is country-specific . Expenditure-based GDP is total final expenditures at purchasers? prices (including the f.o.b. value of exports of goods and services), less the f.o.b. value of imports of goods and services. [SNA 1993]	Percent change		See notes for:  Gross domestic product, constant prices (National currency).	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	8.692	0.671	11.830	5.361	13.340	3.863	20.585	8.438	6.479	13.968	3.933	1.282	2.041	2.984	3.982	4.977	6.016	6.008	2013
+512	AFG	NGDP	Afghanistan	Gross domestic product, current prices	Expressed in billions of national currency units . Expenditure-based GDP is total final expenditures at purchasers? prices (including the f.o.b. value of exports of goods and services), less the f.o.b. value of imports of goods and services. [SNA 1993]	National currency	Billions	Source: National Statistics Office Latest actual data: 2013. Latest actual data is for the solar year 2013/14 Notes: National accounts data is originally compiled on the basis of a solar year, which runs from March 21 to March 20. Data is converted to calendar years for the purpose of WEO publication. National accounts manual used: System of National Accounts (SNA) 1993 GDP valuation: Market prices Start/end months of reporting year: January/December. none Base year: 2002. Base year is the solar year 2002/03 Chain-weighted: No Primary domestic currency: Afghan Afghani Data last updated: 09/2015	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	n/a	178.756	220.013	246.210	304.926	345.817	427.495	517.509	607.227	711.759	836.222	1,033.591	1,116.827	1,173.182	1,172.361	1,240.498	1,333.489	1,468.179	1,632.525	1,815.953	2013
+"""
+
+
+DATASETS['IFS']["data"]="""Country Name,Country Code,Indicator Name,Indicator Code,Time Period,Value,Status
+Australia,193,"Non-standardized Presentation (Monetary Authorities), Central or General Government Deposits, National Currency",16D___XDC,1963,0,
+Ethiopia,644,"Non-standardized Presentation (Banking Survey and Financial Survey), Foreign Assets, Net Foreign Assets, National Currency",51N___XDC,1966,170644999.99523,
+Euro Area,163,"Goods, Deflator/Unit Value of Imports, US Dollars, Index",TMG_D_USD_CIF_IX,2000Q4,90.0584506386,
+Ethiopia,644,"Non-standardized Presentation (Banking Survey and Financial Survey), Foreign Assets, Net Foreign Assets, National Currency",51N___XDC,1968,170844999.99523,
+"Middle East, North Africa, Afghanistan, and Pakistan",440,"Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars",TMG_CIF_USD,1982M2,158961269591.406,
+Ethiopia,644,"Non-standardized Presentation (Banking Survey and Financial Survey), Foreign Assets, Net Foreign Assets, National Currency",51N___XDC,1969,170944999.99523,
+"Armenia, Republic of",911,"Financial account, Other investment, Other equity, Net acquisition of financial assets, US Dollars",BFOAE_BP6_USD,2010,1575884.06509921,
+Ethiopia,644,"Non-standardized Presentation (Banking Survey and Financial Survey), Foreign Assets, Net Foreign Assets, National Currency",51N___XDC,1971,171144999.99523,
+Eastern Caribbean Currency Union (ECCU),309,"Central Government, Cash surplus/deficit, Adjustment to Cash surplus/deficit, 2001 Manual, Cash, National Currency",GCXCCBA_G01_CA_XDC,2008,0,
+Ethiopia,644,"Non-standardized Presentation (Banking Survey and Financial Survey), Foreign Assets, Net Foreign Assets, National Currency",51N___XDC,1970,171044999.99523,
+"Armenia, Republic of",911,"Financial account, Other investment, Other equity, Net acquisition of financial assets, US Dollars",BFOAE_BP6_USD,2009,1566884.06509921,
+Euro Area,163,"Goods, Deflator/Unit Value of Imports, US Dollars, Index",TMG_D_USD_CIF_IX,2000Q2,75.0584506386,
+"Armenia, Republic of",911,"Financial account, Other investment, Other equity, Net acquisition of financial assets, US Dollars",BFOAE_BP6_USD,2011,1579884.06509921,
+"Middle East, North Africa, Afghanistan, and Pakistan",440,"Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars",TMG_CIF_USD,1982M1,158781269591.406,
+"Armenia, Republic of",911,"Financial account, Other investment, Other equity, Net acquisition of financial assets, US Dollars",BFOAE_BP6_USD,2008,1565900.06509921,
+"Middle East, North Africa, Afghanistan, and Pakistan",440,"Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars",TMG_CIF_USD,1982M3,159761269591.406,
+Euro Area,163,"Goods, Deflator/Unit Value of Imports, US Dollars, Index",TMG_D_USD_CIF_IX,2000Q3,80.0584506386,
+"Middle East, North Africa, Afghanistan, and Pakistan",440,"Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars",TMG_CIF_USD,1982M5,160761269591.406,
+Euro Area,163,"Goods, Deflator/Unit Value of Imports, US Dollars, Index",TMG_D_USD_CIF_IX,2000Q1,74.0584506386,
+"Middle East, North Africa, Afghanistan, and Pakistan",440,"Goods, Value of Imports, Cost, Insurance, Freight (CIF), US Dollars",TMG_CIF_USD,1982M4,159961269591.406,
+Ethiopia,644,"Non-standardized Presentation (Banking Survey and Financial Survey), Foreign Assets, Net Foreign Assets, National Currency",51N___XDC,1967,170744999.99523,
+"""
+
